1fee602e0eb21947bfe8dd76b2d758a2655e9027
Wyn <yingwen_92@163.com>
2016-08-15 14:33:34 +0800

    heuristic initialize

--- GA/main.cpp
+++ GA/main.cpp
@@ -15,18 +15,25 @@ int main()
 	rand();
 	//srand((unsigned)time(NULL));
 	int G[DIM][DIM];
-	int pheno_heuristic[DIM], heuristic_flag = 0;
+	int pheno_heuristic[50][DIM];
 	net::BANetworkG(G, 2, 2);
 	double a, b, c = 1, p;
 	unsigned int seed = 1;
+	for (int t = 0; t < 50; t++)
+	{
+		for (int i = 0; i < DIM; i++)
+		{
+			pheno_heuristic[t][i] = 0;
+		}
+	}
+
 	for (a = 1.6; a <1.65; a += 0.1) {
-		for (p = 0.16; p < 0.205; p += 0.01) {
+		for (p = 0; p < 0.205; p += 0.01) {
 			for (b = 0; b < 2.005; b += 0.1)
 			{
 				stringstream txtname;
 				txtname << "power grid fig5 a=1.6 p=" << p << " b="<< b << ".csv";
 				ofstream output(txtname.str(), ios::app);
-				heuristic_flag = 0;
 
 				map<int, int> num_of_degree;
 				COST_TYPE sum_cost = 0;
@@ -40,12 +47,6 @@ int main()
 				}	//tmp = 3994, based on parameter setting m0=m=2.*/
 				sum_cost = 3994 * p;
 
-#ifdef _USE_HEURISTIC_INIT
-				if (heuristic_flag)
-				{
-					ga.HeuristicInit(pheno_heuristic);
-				}
-#endif
 				for (int i = 0; i < 50; i++)
 				{
 					srand(i);	//synchronize 50 networks
@@ -54,15 +55,18 @@ int main()
 					srand(seed + unsigned(time(NULL)));	//rand, use seed+time to avoid too fast
 					seed = rand();
 					GA1 ga(G, sum_cost, a, b, c);
+#ifdef _USE_HEURISTIC_INIT
+					ga.HeuristicInit(pheno_heuristic[i]);
+#endif
 					ga.output = &output;
 					cout << "The " << i + 1 << " times of " << txtname.str() << ". b=" << b << endl;
-					ga.Run(pheno_heuristic);//test
+					ga.Run(pheno_heuristic[i]);//test
 					for (map<int, int>::iterator i = ga.num_of_degree.begin(); i != ga.num_of_degree.end(); i++)
 					{
 						num_of_degree[i->first] += i->second;
 					}
 #ifdef _USE_HEURISTIC_INIT
-					heuristic_flag = 1;
+
 #endif
 				}
 				for (map<int, int>::iterator i = num_of_degree.begin(); i != num_of_degree.end(); i++)
